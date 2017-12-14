b913ad908d4400df9348a8b3d1804363f5096f77
skydoves <skydoves@naver.com>
2017-12-14 18:08:43 +0900

    update JavaDoc & super initializeScopes

--- demo/src/main/java/com/skydoves/methodscopedemo/MainActivity.java
+++ demo/src/main/java/com/skydoves/methodscopedemo/MainActivity.java
@@ -2,8 +2,10 @@ package com.skydoves.methodscopedemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.skydoves.methodscope.MethodScope;
 import com.skydoves.methodscope.ScopeInitializer;
@@ -21,24 +23,31 @@ public class MainActivity extends AppCompatActivity implements ScopeInitializer 
 
     @Override
     public void initializeScopes() {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 
     @InitTestScope
     public void initTestScope_Impl() {
         setContentView(R.layout.activity_test);
         Button button = findViewById(R.id.button);
-        button.setText(text);
+        button.setText(text + "\nhere is TestScope");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                realTest();
+            }
+        });
     }
 
     @InitPowerScope
     public void initPowerScope_Impl() {
         setContentView(R.layout.activity_power);
         TextView textView = findViewById(R.id.textView);
-        textView.setText(text);
+        textView.setText(text + "\nhere is PowerScope");
     }
 
     @TestScope
     public void realTest() {
-
+        Toast.makeText(this, "realTest invoked!", Toast.LENGTH_SHORT).show();
     }
 }
