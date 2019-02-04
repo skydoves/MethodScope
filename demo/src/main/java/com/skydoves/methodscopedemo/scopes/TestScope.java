864b89814c72d3c5ad558b1415b791fe97254140
skydoves <skydoves@naver.com>
2019-02-04 16:03:08 +0900

    redesigned @MethodScope annotation

---
+++ demo/src/main/java/com/skydoves/methodscopedemo/scopes/TestScope.java
@@ -0,0 +1,14 @@
+package com.skydoves.methodscopedemo.scopes;
+
+import com.airbnb.deeplinkdispatch.DeepLink;
+import com.skydoves.methodscope.MethodScope;
+
+/**
+ * Developed by skydoves on 2019-02-04.
+ * Copyright (c) 2018 skydoves rights reserved.
+ */
+
+@MethodScope
+public @interface TestScope {
+    DeepLink deeplink();
+}
