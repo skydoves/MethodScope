bdce6ce9c6715f7e6de49561ed6d25198de670e4
skydoves <skydoves@naver.com>
2019-02-05 01:54:18 +0900

    implements attaches annotations by scope & super. scoped method

--- demo/src/main/java/com/skydoves/methodscopedemo/MainActivity.kt
+++ demo/src/main/java/com/skydoves/methodscopedemo/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.skydoves.methodscopedemo
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.deeplinkdispatch.DeepLink
 import com.skydoves.methodscopedemo.scopes.MyScope
@@ -10,21 +11,25 @@ import com.skydoves.methodscopedemo.scopes.TestScope
 @TestScope(deeplink = DeepLink("https://www.naver.com"))
 open class MainActivity : AppCompatActivity() {
 
+    var hello = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initialize()
     }
 
     open fun initialize() {
-
+        hello = "This is "
     }
 
-    fun initialize_TestScope() {
-
+    fun initializeTestScope() {
+        hello += "TestScope"
+        Toast.makeText(this, hello, Toast.LENGTH_LONG).show()
     }
 
-    fun initialize_MyScope() {
-
+    fun initializeMyScope() {
+        hello += "MyScope"
+        Toast.makeText(this, hello, Toast.LENGTH_LONG).show()
     }
 
     companion object {
